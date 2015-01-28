3683773bde331b00f65d76ae70f17994576a3115
Michael Vorburger <mike@vorburger.ch>
2015-01-28 09:30:50 +0100

    [tests] DumpParsedStructureUtil

--- tests/org.eclipse.emf.eson.tests/src/org/eclipse/emf/eson/builder/tests/ReferenceCrossResourceTest.java
+++ tests/org.eclipse.emf.eson.tests/src/org/eclipse/emf/eson/builder/tests/ReferenceCrossResourceTest.java
@@ -2,7 +2,7 @@
  * #%L
  * org.eclipse.emf.eson.tests
  * %%
- * Copyright (C) 2013 - 2014 Michael Vorburger
+ * Copyright (C) 2013 - 2015 Michael Vorburger
  * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,6 +14,7 @@ package org.eclipse.emf.eson.builder.tests;
 
 import javax.inject.Inject;
 
+import org.eclipse.emf.eson.tests.util.DumpParsedStructureUtil;
 import org.eclipse.emf.eson.tests.util.ESONWithTestmodelInjectorProvider;
 import org.eclipse.emf.eson.tests.util.ResourceProvider;
 import org.eclipse.xtext.junit4.InjectWith;
@@ -28,13 +29,21 @@ import testmodel.TestModel;
 @InjectWith(ESONWithTestmodelInjectorProvider.class)
 public class ReferenceCrossResourceTest {
 
+	@Inject DumpParsedStructureUtil dumper;
 	@Inject ResourceProvider provider;
 
 	@Test public void testCrossResourceReference() throws Exception {
-		/*TestModel aModel =*/ provider.loadModel("res/BuilderTests/ReferenceCrossResourceTestA.efactory", TestModel.class);
-		TestModel bModel = provider.loadModel("res/BuilderTests/ReferenceCrossResourceTestB.efactory", TestModel.class);
+		TestModel aModelUnvalidated = provider.loadModel("res/BuilderTests/ReferenceCrossResourceTestA.efactory", TestModel.class, false);
+		dumper.dump(aModelUnvalidated);
+		provider.validate(aModelUnvalidated.eResource());
+
+		TestModel bModel = provider.loadModel("res/BuilderTests/ReferenceCrossResourceTestB.efactory", TestModel.class, false);
+		dumper.dump(bModel);
+		provider.validate(bModel.eResource());
+		
 		Assert.assertEquals("1-target,1", bModel.getReferenceTestContainer().get(0).getReferenceToOne().getName());
 		Assert.assertEquals("4", bModel.getReferenceTestContainer().get(0).getReferenceToMany().get(3).getName());
+		// TODO Assert.assertEquals("target31", bModel.getReferenceTestContainer().get(1).getReferenceToMany().get(3).getName());
 	}
 	
 }
---
+++ tests/org.eclipse.emf.eson.tests/src/org/eclipse/emf/eson/tests/util/DumpParsedStructureUtil.java
@@ -0,0 +1,96 @@
+package org.eclipse.emf.eson.tests.util;
+
+import javax.inject.Inject;
+
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.eson.eFactory.Containment;
+import org.eclipse.emf.eson.eFactory.EnumAttribute;
+import org.eclipse.emf.eson.eFactory.Factory;
+import org.eclipse.emf.eson.eFactory.Feature;
+import org.eclipse.emf.eson.eFactory.MultiValue;
+import org.eclipse.emf.eson.eFactory.NewObject;
+import org.eclipse.emf.eson.eFactory.Reference;
+import org.eclipse.emf.eson.eFactory.Value;
+import org.eclipse.emf.eson.resource.EFactoryResource;
+import org.eclipse.emf.eson.util.XtextProxyUtil;
+
+public class DumpParsedStructureUtil {
+	
+	protected @Inject XtextProxyUtil xtextProxyUtil;  
+
+	public void dump(EObject eo) {
+		Resource eResource = eo.eResource();
+		final EFactoryResource eFactoryResource = (EFactoryResource) eResource;
+		Factory factory = eFactoryResource.getEFactoryFactory();
+		if (factory == null)
+			return;
+		dump("", factory.getRoot());
+	}
+	
+	protected void dump(String indent, NewObject no) {
+		dumpPossiblyNamedOrProxy(no, no.getEClass());
+		if (no.getName() != null) {
+			System.out.print(" '");
+			System.out.print(no.getName());
+			System.out.print("'");
+		}
+		System.out.print(" ");
+		System.out.print("{");
+		if (!no.getFeatures().isEmpty())
+			System.out.println();
+		
+		for (Feature feature : no.getFeatures()) {
+			System.out.print(indent + "    ");
+			dumpPossiblyNamedOrProxy(feature, feature.getEFeature());
+			System.out.print(": ");
+			dumpValue(indent + "    ", feature.getValue(), no);
+		}
+		
+		if (!no.getFeatures().isEmpty())
+			System.out.print(indent);
+		System.out.println("}");
+	}
+
+	protected void dumpPossiblyNamedOrProxy(EObject ctx, EObject eObject) {
+		if (!eObject.eIsProxy()) {
+			if (eObject instanceof ENamedElement) {
+				ENamedElement namedElement = (ENamedElement) eObject;
+				System.out.print(namedElement.getName());
+			}
+			else
+				System.out.print(eObject.toString());
+		} else {
+			System.out.print("PROXY-");
+			System.out.print(xtextProxyUtil.getProxyCrossRefAsString(ctx, eObject));
+		}
+	}
+	
+	protected void dumpValue(String indent, Value value, EObject ctx) {
+		if (value == null) {
+			System.out.println("!Feature-Value-isNull");
+		} else if (value instanceof Containment) {
+			Containment containment = (Containment) value;
+			dump(indent, containment.getValue());
+		} else if (value instanceof MultiValue) {
+			System.out.println("[");
+			MultiValue mv = (MultiValue) value;
+			for (Value avalue : mv.getValues()) {
+				System.out.print(indent + "    ");
+				dumpValue(indent + "    ", avalue, mv);
+			}
+			System.out.println(indent + "]");
+		} else if (value instanceof Reference) {
+			Reference ref = (Reference) value;
+			System.out.print("#");
+			dumpPossiblyNamedOrProxy(ctx, ref.getValue());
+			System.out.println();
+		} else if (value instanceof EnumAttribute) {
+			EnumAttribute enumAttribute = (EnumAttribute) value;
+			System.out.println(indent + "ENUM " + enumAttribute.getValue().toString());
+		} else {
+			throw new IllegalStateException("Implement instanceof for: " + value.getClass());
+		}
+	}
+}
