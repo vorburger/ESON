ca7c690cce066b637da05b9041cc71fdbe1957b7
Michael Vorburger <mike@vorburger.ch>
2015-01-29 15:52:02 +0100

    [bugfix] Obvious typo in NodeFixer (found by chance while working on NewGrammarWithAnyValue branch)

--- plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/serialization/NodeFixer.java
+++ plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/serialization/NodeFixer.java
@@ -19,14 +19,14 @@ import java.util.List;
 import javax.inject.Singleton;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.eson.eFactory.EFactoryPackage;
+import org.eclipse.emf.eson.services.EFactoryGrammarAccess;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.impl.NodeModelBuilder;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 
 import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
-import org.eclipse.emf.eson.eFactory.EFactoryPackage;
-import org.eclipse.emf.eson.services.EFactoryGrammarAccess;
 
 /**
  * Adds missing INode to new AST objects created in EFactoryAdapter.
@@ -60,7 +60,7 @@ public class NodeFixer {
 			else if (astElement.eClass().equals(EFactoryPackage.Literals.STRING_ATTRIBUTE))
 				associateNode(astElement, grammarAccess.getStringAttributeRule());
 			else if (astElement.eClass().equals(EFactoryPackage.Literals.REFERENCE))
-				associateNode(astElement, grammarAccess.getNullAttributeRule());
+				associateNode(astElement, grammarAccess.getReferenceRule());
 			else if (astElement.eClass().equals(EFactoryPackage.Literals.ENUM_ATTRIBUTE))
 				associateNode(astElement, grammarAccess.getEnumAttributeRule());
 			else if (astElement.eClass().equals(EFactoryPackage.Literals.INTEGER_ATTRIBUTE))
