1421235d5d5e2a458e38a2f77b0c00582356c4b9
Michael Vorburger <mike@vorburger.ch>
2015-11-13 17:44:44 +0100

    [feature] IGenerator from plugin.xml extension point, TDD (DS-9315)
    incl. illustration in example project 
    also test for IGenerator from plug-in
    TDD testXtextBuilderWithGeneratorInPlugin
    testXtextBuilderWithGeneratorInRuntimeWorkspace green, with dyn EMF
    org.eclipse.emf.eson.example.generator.Generator w. dynamic example
    catch, clearly log and rethrow ServiceLoader (ClassLoader) exceptions
    
    see
    http://stackoverflow.com/questions/35044472/how-to-enable-install-the-compatibility-layer-in-maven-tycho-surefire-so-that
    about changes required in addPlatformJarToClasspath()
    
    Change-Id: I2ed1fef4254bef58eb10c5889d8073c5457defa4

--- plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/generators/DelegatingGenerator.java
+++ plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/generators/DelegatingGenerator.java
@@ -2,7 +2,7 @@
  * #%L
  * org.eclipse.emf.eson
  * %%
- * Copyright (C) 2015 Michael Vorburger
+ * Copyright (C) 2015 - 2016 Michael Vorburger
  * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,7 @@
  */
 package org.eclipse.emf.eson.generators;
 
+import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 
 import org.apache.log4j.Logger;
@@ -24,37 +25,57 @@ import com.google.inject.Inject;
 /**
  * An IGenerator which delegates to other IGenerator listed in a
  * META-INF/services/org.eclipse.xtext.generator.IGenerator on the classpath of
- * the project of the input Resource being generated.
- * 
- * TODO Also support looking up generators from other plug-ins using an
- * extension point instead of only the run-time classpath? See à la
- * com.temenos.ds.op.xtext.generator.ui.IGeneratorsProvider, and
- * com.temenos.ds.op.xtext.generator.ui.PluginsGeneratorsProvider.
+ * the project of the input Resource being generated, or via the Eclipse plugin
+ * system.
  * 
  * @author Michael Vorburger, based on ClassLoader magic tips from Stefan Oehme
  */
 public class DelegatingGenerator implements IGenerator {
 	private static final Logger logger = Logger.getLogger(DelegatingGenerator.class);
 
-	// TODO Integration Test!! @see com.temenos.ds.op.xtext.generator.tests.MultiGeneratorXtextBuilderParticipantTest
-
 	protected @Inject ClassLoaderProvider classLoaderProvider;
+	protected @Inject EFactoryExtensionRegistry registryReaderUtil;
 	
 	@Override
 	public void doGenerate(Resource input, IFileSystemAccess fsa) {
+		delegateToProjectRuntimeClasspathGenerators(input, fsa);
+		delegateToPluginGenerators(input, fsa);
+	}
+
+	protected void delegateToPluginGenerators(Resource input, IFileSystemAccess fsa) {
+		Iterable<IGenerator> generators = registryReaderUtil.getGenerators();
+		invokeGeneratorsAndHandleErrors(input, fsa, generators, "project runtime classpath generator ");
+	}
+
+	protected void delegateToProjectRuntimeClasspathGenerators(Resource input, IFileSystemAccess fsa) {
 		ClassLoader classLoader = classLoaderProvider.getClassLoader(input);
-		Iterable<IGenerator> generators = getGenerators(classLoader);
-		for (IGenerator generator : generators) {
-			try {
- 				generator.doGenerate(input, fsa);
-			} catch (Exception e) {
-				logger.error("Caught exception from doGenerate() of " + generator.toString() + " for URI: " + input.getURI(), e);
+		Iterable<IGenerator> generators = getRuntimeGenerators(classLoader);
+		invokeGeneratorsAndHandleErrors(input, fsa, generators, "project runtime classpath generator ");
+	}
+
+	protected void invokeGeneratorsAndHandleErrors(Resource input, IFileSystemAccess fsa, Iterable<IGenerator> generators, String errorMessagePrefix) {
+		try {
+			for (IGenerator generator : generators) {
+				try {
+	 				generator.doGenerate(input, fsa);
+				} catch (RuntimeException e) {
+					logger.error("Caught exception from doGenerate() of " + errorMessagePrefix + generator.toString() + " for URI: " + input.getURI(), e);
+					throw e;
+				}
 			}
+		} catch (ServiceConfigurationError e) {
+			logger.error("Caught exception when iterating over ServiceLoader", e);
+			throw e;
 		}
 	}
 
-	protected ServiceLoader<IGenerator> getGenerators(ClassLoader classLoader) {
-		return ServiceLoader.load(IGenerator.class, classLoader); // TODO Perhaps this should be cached?
+	protected ServiceLoader<IGenerator> getRuntimeGenerators(ClassLoader classLoader) {
+		try {
+			return ServiceLoader.load(IGenerator.class, classLoader);
+		} catch (ServiceConfigurationError e) {
+			logger.error("Caught exception when obtaining project runtime classpath generators", e);
+			throw e;
+		}
 	}
 
 }
